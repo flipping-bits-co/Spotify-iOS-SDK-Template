9b5e4227eb06ed494a4909c1175d1dac24028410
Fernando Balandran <fernandobe+git@protonmail.com>
2024-09-16 16:26:06 -0500

    init

---
+++ Spotify iOS SDK Template/SpotifyiOS.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_iOS_SDK_TemplateApp.swift
+//  Spotify iOS SDK Template
+//
+//  Created by Fernando Balandran on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyiOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
